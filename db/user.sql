404832cb6af25816b66b4c6e87f67855737a7b6f
txsminn <txsminn@gmail.com>
2025-03-07 11:24:48 +0800

    Update user.sql

--- db/user.sql
+++ db/user.sql
@@ -1,13 +1,17 @@
--- Create the users table
-CREATE TABLE IF NOT EXISTS user (
+CREATE DATABASE IF NOT EXISTS user;
+USE user;
+
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Insert sample users
-INSERT INTO users (name, email) VALUES 
-('Alice Johnson', 'alice@example.com'),
-('Bob Smith', 'bob@example.com'),
-('Charlie Brown', 'charlie@example.com');
+INSERT INTO users (name, email, password) VALUES 
+('Alice Johnson', 'alice@example.com','dummy_pw'),
+('Bob Smith', 'bob@example.com','testpw'),
+('Charlie Brown', 'charlie@example.com', 'dummy_pw');
